6c090aa9a69de50fa98da50956bbfd1c991e8cac
DrSzuriad <43809250+DrSzuriad@users.noreply.github.com>
2024-07-01 10:24:28 +0200

    Update translation to 1.7.1

--- pull_translations_from_transifex.bat
+++ pull_translations_from_transifex.bat
@@ -1,4 +1,8 @@
 echo "Pulling localisation files from Transifex"
+
+echo "Usunięcie starych plików."
+rd /s /q "pliki\\pl\\"
+
 :: Force pull all translations
 tx.exe pull --all --force -translations
 pause
